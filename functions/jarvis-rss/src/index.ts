54bfaeae740b00e7f96d0d212292db7c23e59ff1
Kevin Durbin <kevindurb@fastmail.com>
2024-10-25 08:49:37 -0600

    add basics for getting rss feeds

--- functions/jarvis-rss/src/index.ts
+++ functions/jarvis-rss/src/index.ts
@@ -1,1 +1,27 @@
-console.log('here');
+import http from 'node:http';
+import Parser from 'rss-parser';
+
+const parser = new Parser();
+
+const feedToMarkdown = (feed: Parser.Output<Record<string, unknown>>) => {
+  return `# ${feed.title}
+${feed.items
+  .map(
+    (item) => `## ${item.title}
+${item.content}`,
+  )
+  .join('\n')}`;
+};
+
+const server = http.createServer(async (_, res) => {
+  const feed = await parser.parseURL('https://techcrunch.com/feed/');
+  const markdown = feedToMarkdown(feed);
+
+  console.log(markdown);
+
+  res.end();
+});
+
+server.listen(process.env['PORT'], () => {
+  console.log('Listening...', server.address());
+});
